675f2e569dc6d9b44cdaa40b1f3ea9b5814b0830
Qaanit Khan <qaanitumar771@gmail.com>
2025-10-10 23:31:45 +0530

    Binary Search

--- 10 BINARY SEARCH/_10_TwoSum.java
+++ 10 BINARY SEARCH/_10_TwoSum.java
@@ -1,23 +1,44 @@
 import java.util.Arrays;
 
 public class _10_TwoSum {
+//    public static void main(String[] args) {
+//        int[] arr = {4,3,2,8,0,3,5};
+//        int target = 11;
+//        Arrays.sort(arr);
+//        int i = 0;
+//        int j = arr.length-1;
+//        while(i<j){
+//            if(arr[i]+arr[j] == target){
+//                System.out.print("The sum of "+arr[i]+" "+arr[j]);
+//                break;
+//            }
+//            if(arr[i]+arr[j] > target){
+//                j--;
+//            }
+//            else{
+//                i++;
+//            }
+//        }
+//    }
     public static void main(String[] args) {
         int[] arr = {4,3,2,8,0,3,5};
         int target = 11;
         Arrays.sort(arr);
         int i = 0;
         int j = arr.length-1;
-        while(i<j){
+        while(i<=j){
+            int mid = (i+j)/2;
             if(arr[i]+arr[j] == target){
-                System.out.print("The sum of "+arr[i]+" "+arr[j]);
+                System.out.print(arr[i]+" "+arr[j]);
                 break;
             }
-            if(arr[i]+arr[j] > target){
+            else if(arr[i]+arr[j] > target){
                 j--;
             }
             else{
                 i++;
             }
         }
+
     }
 }
