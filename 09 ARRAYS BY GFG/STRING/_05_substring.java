f4e3b9f4575d4bb668331a9cead2f0dfa428d263
Qaanit Khan <qaanitumar771@gmail.com>
2026-02-11 00:18:26 +0530

    String Builder and String methods

--- 09 ARRAYS BY GFG/STRING/_05_substring.java
+++ 09 ARRAYS BY GFG/STRING/_05_substring.java
@@ -1,11 +1,10 @@
 public class _05_substring {
     public static void main(String[] args) {
         String n1 = "Qaanit";
-        int n = n1.length();
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<=n;j++){
-                System.out.println(n1.substring(i,j));
-            }
+        for(int i = 0;i<n1.length();i++){
+            for(int j = i;j<n1.length();j++){
+            System.out.println(n1.substring(i, j+1));
+        }
         }
     }
 }
---
+++ 09 ARRAYS BY GFG/STRING/_06_StringBuilder.java
@@ -0,0 +1,13 @@
+
+
+public class _06_StringBuilder {
+    public static void main(String[] args) {
+
+        // ArrayList<Character> k = new ArrayList<>();
+        // k.add('e');
+
+        StringBuilder str = new StringBuilder();
+        str.append("Qaanit Khan");
+        System.out.println(str.capacity() +" "+str.length());
+    }
+}
