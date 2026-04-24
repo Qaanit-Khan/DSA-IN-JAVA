8cbdd7e41a44b2c50398a04b869b0637808e73c0
Qaanit Khan <qaanitumar771@gmail.com>
2026-04-24 15:23:22 +0530

    Recursion

--- 09 ARRAYS BY GFG/1D Array/_01_PassingArrayToMethod.java
+++ 09 ARRAYS BY GFG/1D Array/_01_PassingArrayToMethod.java
@@ -3,8 +3,7 @@ public class _01_PassingArrayToMethod {
         arr2[1] = 222;
     }
     public static void main(String[] args){
-        int[] arr = {202,221,213,343};
-        System.out.println(arr[1]);
+        
         Change(arr);
         System.out.println(arr[1]);
     }
--- 11 RECURSION/_09_hcf.java
+++ 11 RECURSION/_09_hcf.java
@@ -1,8 +1,12 @@
 public class _09_hcf {
     public static int HCF(int a,int b){
-        
+        return helper(Math.min(a, b),Math.max(a, b));
+    }
+    public static int helper(int a,int b){
+        if(a == 0) return b;
+        return helper(b%a,a);
     }
     public static void main(String[] args) {
-        HCF(a, b);
+        System.out.println(HCF(2, 10));
     }    
 }
